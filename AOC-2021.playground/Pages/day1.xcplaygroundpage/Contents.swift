02af49918eca165d5760f99894d085b66e60c013
Rajiv Jhoomuck <rajivjhoomuck@icloud.com>
2021-12-03 00:25:45 +0100

    Day 1 - Part 2

--- AOC-2021.playground/Pages/day1.xcplaygroundpage/Contents.swift
+++ AOC-2021.playground/Pages/day1.xcplaygroundpage/Contents.swift
@@ -1,7 +1,12 @@
 import Foundation
 
-let parsedInts = try! load(fileNamed: "data", withExtension: "txt")
-  .lines
-  .compactMap(Int.init)
+//: # Part 1
+let parsedInts = try! load(fileNamed: "data", withExtension: "txt").lines.compactMap(Int.init)
+let partOneAnswer = zip(parsedInts[...], parsedInts[1...]).map(<).filter(id).count
 
-let answer = zip(parsedInts[...], parsedInts[1...]).map(<).filter(id).count
+//: Part 2
+//: > Assuming at least 3 lines, groupings = count - 2
+let groupingsCount = parsedInts.count - 2
+let groupedSum = Array(0..<groupingsCount)
+  .map({ parsedInts[$0...($0+2)].reduce(0, +) })
+let partTwoAnswer = zip(groupedSum[...], groupedSum[1...]).map(<).filter(id).count
