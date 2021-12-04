57b4cef50a5123f6d709f2904a978d11a78a4e39
Rajiv Jhoomuck <rajivjhoomuck@icloud.com>
2021-12-04 21:16:54 +0100

    finito first part

--- AOC-2021.playground/Pages/day4.xcplaygroundpage/Contents.swift
+++ AOC-2021.playground/Pages/day4.xcplaygroundpage/Contents.swift
@@ -1,1 +1,41 @@
 import Foundation
+
+struct Board {
+  let columns: [[Int]]
+  let rows: [[Int]]
+  init(rawValue: String) {
+    self.rows = rawValue
+      .split(separator: "\n")
+      .map({ $0.split(separator: " ").compactMap(String.init >>> Int.init) })
+    self.columns = transpose(rows)
+  }
+}
+
+/// Creates all draw events from provided input
+func _draws(_ input: [Int]) -> [[Int]] { (4..<input.count).map({ Array(input[0...$0]) }) }
+
+func gameStats(fromFile fileName: String) -> ([[Int]], [Board]) {
+  let output = try! load(fileNamed: fileName).components(separatedBy: "\n\n")
+  let drawSequences = output[0].split(separator: ",").compactMap(String.init >>> Int.init) |> _draws
+  let boards = output.dropFirst().map(Board.init)
+  return (drawSequences, boards)
+}
+
+func firstWinningBoard(with draws: [Int], and boards: [Board]) -> ([Int], Board)? {
+  guard
+    let board = boards.first (where: { _board in
+      (_board.rows + _board.columns).map({ Set(draws).isSuperset(of: $0) }).reduce(false, { $0 || $1 })
+    }) else { return nil }
+  return (draws, board)
+}
+
+func calculateScore(draw: [Int], board: Board) -> Int {
+  draw.reversed()[0] * board.rows.flatMap(id).filter({ !draw.contains($0) }).reduce(0, +)
+}
+
+let (drawSequences, boards) = gameStats(fromFile: "data")
+
+//: * Experiment: Part One Answer
+let partOneAnswer = zip(drawSequences, Array(repeating: boards, count: drawSequences.count))
+  .compactMap { firstWinningBoard(with: $0, and: $1) }
+  .first.map { calculateScore(draw: $0, board:$1) }
--- AOC-2021.playground/Sources/Utils.swift
+++ AOC-2021.playground/Sources/Utils.swift
@@ -1,13 +1,14 @@
 import Foundation
 
+public func load(fileNamed name: String) throws -> String {
+  try Bundle.main.url(forResource: name, withExtension: "txt").map({ try String.init(contentsOf:$0) })!
+}
+
 public func load<A>(
   fileNamed name: String,
   lineTransform: (String) -> A?
 ) throws -> [A] {
-  guard let url = Bundle.main.url(forResource: name, withExtension: "txt")
-  else { throw "Invalid url" }
-
-  return try String(contentsOf: url).lines.compactMap(lineTransform)
+  try load(fileNamed: name).lines.compactMap(lineTransform)
 }
 
 public extension String {
@@ -17,3 +18,13 @@ public extension String {
 extension String: Error {}
 
 public let parseIntFromBinaryStringRep: (String) -> Int? = 2 |> flip(curry(Int.init(_: radix:)))
+
+public extension Array {
+  var head: Element { self[0] }
+  var tail: [Element] { Self(self[1...]) }
+}
+
+public func transpose<A>(_ input: [[A]]) -> [[A]] {
+  guard !input.head.isEmpty else { return [] }
+  return [input.map(\.head)] + transpose(input.map(\.tail))
+}
