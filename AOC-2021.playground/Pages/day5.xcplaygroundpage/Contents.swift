610a1b4c66091fc2167bb96ebb4befb42f1f4086
Rajiv Jhoomuck <rajivjhoomuck@icloud.com>
2021-12-05 20:27:28 +0100

    Finito part 2

--- AOC-2021.playground/Pages/day5.xcplaygroundpage/Contents.swift
+++ AOC-2021.playground/Pages/day5.xcplaygroundpage/Contents.swift
@@ -22,21 +22,36 @@ struct Line {
     } else { return [] }
   }
 
+  var diagonalPoints: [Point] {
+    guard abs(start.x - end.x) == abs(start.y - end.y) else { return [] }
+    let xrange = makeRange(i: start.x, j: end.x)
+    let yrange = makeRange(i: start.y, j: end.y)
+    return zip(xrange, yrange).map(Point.init(x: y:))
+  }
+
+  var all: [Point] { verticalOrHorizontalPoints + diagonalPoints }
+
   init(coords: [Point]) { (self.start, self.end) = (coords[0], coords[1]) }
 
-  func makeRange(i: Int, j: Int) -> [Int] { Array(i <= j ? i...j : j...i) }
+  func makeRange(i: Int, j: Int) -> [Int] { i <= j ? Array(i...j) : Array(j...i).reversed() }
+  // When LT, make the range by reversing, since closed range constructor need first arg to be lower than second arg (bounds)
 }
 
-func parsePointsOnHVLines(_ input: String) -> [Point] {
+func parsePoints(includingDiagonals diagionals: Bool, input: String) -> [Point] {
   input.components(separatedBy: " -> ").map(Point.init(input:))
-    |> Line.init(coords:) >>> \Line.verticalOrHorizontalPoints
+    |> Line.init(coords:) >>> (diagionals ? { $0.all } : { $0.verticalOrHorizontalPoints })
+}
+
+func dangerPointsCount(from fileName: String, parsePoints: (String) -> [Point]) throws -> Int {
+  try load(fileNamed: fileName)
+    .lines
+    .reduce(into: [Point: [Point]]()) { acc, str in
+      acc.merge(Dictionary(grouping: parsePoints(str), by: id), uniquingKeysWith: +)
+    }
+    .filter(\.1.count >>> compare(>, 1))
+    .count
 }
 
 //: * Experiment: Answers
-let partOneAnswer = try! load(fileNamed: "data")
-  .lines
-  .reduce(into: [Point: [Point]]()) { acc, str in
-    acc.merge(Dictionary(grouping: parsePointsOnHVLines(str), by: id), uniquingKeysWith: +)
-  }
-  .filter(\.1.count >>> compare(>, 1))
-  .count
+let partOneAnswer = try! dangerPointsCount(from: "data", parsePoints: false |> curry(parsePoints(includingDiagonals:input:)))
+let partTwoAnswer = try! dangerPointsCount(from: "data", parsePoints: true |> curry(parsePoints(includingDiagonals:input:)))
