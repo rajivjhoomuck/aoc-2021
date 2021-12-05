30accb7deeba776062477bbdf4ae79a74f1e5250
Rajiv Jhoomuck <rajivjhoomuck@icloud.com>
2021-12-05 01:05:46 +0100

    Minor refactoring

--- AOC-2021.playground/Pages/day1.xcplaygroundpage/Contents.swift
+++ AOC-2021.playground/Pages/day1.xcplaygroundpage/Contents.swift
@@ -1,10 +1,9 @@
 import Foundation
 
-//: # Part 1
-let parsedInts = try! load(fileNamed: "data", lineTransform: Int.init)
-let partOneAnswer = zip(parsedInts[...], parsedInts[1...]).map(<).filter(id).count
+func zipped<A>(xs: [A]) -> Zip2Sequence<ArraySlice<A>, ArraySlice<A>> { zip(xs[...], xs[1...]) }
 
-//: # Part 2
+let parsedInts = try! load(fileNamed: "data", lineTransform: Int.init)
+let partOneAnswer = zipped(xs: parsedInts).map(<).filter(id).count
 //: > Assuming at least 3 lines, groupings = count - 2
 let groupedSum = Array(0..<(parsedInts.count - 2)).map({ parsedInts[$0...($0+2)].reduce(0, +) })
-let partTwoAnswer = zip(groupedSum[...], groupedSum[1...]).map(<).filter(id).count
+let partTwoAnswer = zipped(xs: groupedSum).map(<).filter(id).count
