05746b5ad79d78d05607ca4521396661d1092688
Rajiv Jhoomuck <rajivjhoomuck@icloud.com>
2021-12-07 17:04:17 +0100

    Fixed with help from @edvincandon Needed to trim newline

--- AOC-2021.playground/Pages/day6.xcplaygroundpage/Contents.swift
+++ AOC-2021.playground/Pages/day6.xcplaygroundpage/Contents.swift
@@ -1,7 +1,8 @@
-let data = try! load(fileNamed: "data").split(separator: ",").compactMap(substring2Int)
+let data = try! load(fileNamed: "data").trimmingCharacters(in: .whitespacesAndNewlines).split(separator: ",").compactMap(substring2Int)
 let sample = [3,4,3,1,2]
 let initials = (0...8).reduce(into: [Int: Int](), { acc, key in acc[key] = 0 })
 let counts = Dictionary(grouping: data, by: id).mapValues(\.count).merging(initials, uniquingKeysWith: +)
+
 let partOneAnswer = (0..<80).reduce(into: counts, { acc, _ in
   var buffer = [Int: Int]()
   for (key, value) in acc {
@@ -14,4 +15,20 @@ let partOneAnswer = (0..<80).reduce(into: counts, { acc, _ in
   }
   acc = buffer
 })
+
+
+let bblele = (0..<80).reduce(into: counts, { acc, _ in
+  let head = acc[0] ?? 0
+  var buffer = [Int: Int]()
+
+  for x in 0...7 { buffer[x] = acc[x + 1]; }
+  buffer[6] = (buffer[6] ?? 0) + head;
+  buffer[8] = head;
+
+  acc = buffer
+})
 partOneAnswer.values.reduce(0, +)
+bblele.values.reduce(0, +)
+
+data.reduce(0, +)
+print(data)
